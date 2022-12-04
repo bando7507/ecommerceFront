521b910e0e414218754898216951a610c1e15137
bando7507 <bando7507@gmail.com>
2022-12-04 16:12:58 +0100

    section produits

--- js/app.js
+++ js/app.js
@@ -2,23 +2,23 @@ const navOpen = document.querySelector('.nav__toggle')
 const navClose = document.querySelector('.nav__close')
 const navMenu = document.querySelector('.nav__menu')
 
-navOpen.addEventListener('click', () =>{
+navOpen.addEventListener('click', () => {
     navMenu.classList.add('show')
 })
 
 
-navClose.addEventListener('click', () =>{
+navClose.addEventListener('click', () => {
     navMenu.classList.remove('show')
 })
 
 var homeSwiper = new Swiper(".home-swiper", {
-    spaceBetween : 16,
+    spaceBetween: 16,
     slidesPerView: 'auto',
     centeredSlides: true,
-    loop: 'true', 
+    loop: 'true',
     slidesPerView: 3,
     navigation: 'true',
     pagination: {
         el: ".swiper-pagination",
-      },
+    },
 });
