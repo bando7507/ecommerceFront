57b113ef1be3b520e3f06f711b3357e162d3c5aa
bando7507 <bando7507@gmail.com>
2022-12-17 22:54:45 +0100

    ajout d animation au scroll

--- js/app.js
+++ js/app.js
@@ -16,8 +16,8 @@ navClose.addEventListener('click', () => {
 })
 
 
-allLinks.forEach((el) =>{
-    el.addEventListener('click', () =>{
+allLinks.forEach((el) => {
+    el.addEventListener('click', () => {
         navMenu.classList.remove('show')
     })
 })
@@ -32,14 +32,14 @@ var homeSwiper = new Swiper(".home-swiper", {
     pagination: {
         el: ".swiper-pagination",
     },
-    breakpoints:{
-        0:{
+    breakpoints: {
+        0: {
             slidesPerView: 1
         },
-        525:{
+        525: {
             slidesPerView: 2
         },
-        950:{
+        950: {
             slidesPerView: 3
         }
     }
@@ -50,51 +50,148 @@ var swiper = new Swiper(".myreviewSwiper", {
     spaceBetween: 25,
     slidesPerGroup: 3,
     loop: true,
-    centerSlide:'true',
-    fade:'true',
+    centerSlide: 'true',
+    fade: 'true',
     grabCursor: 'true',
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-    breakpoints:{
-        0:{
+    breakpoints: {
+        0: {
             slidesPerView: 1
         },
-        525:{
+        525: {
             slidesPerView: 2
         },
-        950:{
+        950: {
             slidesPerView: 3
         }
     }
-  });
+});
 
 
 let index = 0;
-
-onglet.forEach((el) =>{
-    el.addEventListener('click', () =>{
+onglet.forEach((el) => {
+    el.addEventListener('click', () => {
         index = el.getAttribute('data-lo')
-
-        for(let i = 0; contenu.length > i; i++ ){
+        for (let i = 0; contenu.length > i; i++) {
             const ll = contenu[i].getAttribute('data-lo')
-           if(ll == index){
-            contenu[i].classList.add('acti')
-           }
-           else{
-            contenu[i].classList.remove('acti')
-           }
+            if (ll == index) {
+                contenu[i].classList.add('acti')
+            } else {
+                contenu[i].classList.remove('acti')
+            }
         }
     })
 })
 
 
+// const circle = document.querySelector('.circle')
+// const homeLeft = document.querySelector('.home__left')
+// const homeImg = document.querySelector('.home__img')
+// const tl = new TimelineMax()
 
 
 
+// tl.fromTo(circle, 1,{
+//     height: '0%'   
+// },{
+//     height: '100%'   
+// })
+
+// tl.fromTo(homeImg, 1.2,{
+//     scale: '.8',
+// },{
+//     scale: '1',
+//     ease: Power2.easeInOut
+// }, '-=.9')
+
+// tl.fromTo(homeLeft, 1,{
+//     x: '150px', 
+//     scale: '.5',
+//     opacity: '0'
+// },{
+//     x: '0',
+//     scale: '1',
+//     opacity: '1',
+//     // ease: Power2.easeInOut
+// }, '-=.9')
+
+
+
+ScrollReveal()
+    .reveal('.virtual', {
+        duration: 1000,
+        delay: 400,
+        distance: "50%",
+        origin: "top",
+        reset: true
+    });
+
+    ScrollReveal()
+    .reveal('.feature__title', {
+        duration: 1000,
+        delay: 400,
+        distance: "10%",
+        origin: "top",
+        reset: true
+    });
+
+ScrollReveal()
+    .reveal('.home__left', {
+        duration: 1500,
+        delay: 500,
+        distance: "30%",
+        origin: "right",
+        reset: true
+    });
+
+ScrollReveal()
+    .reveal('.circle', {
+        duration: 1000,
+        delay: 400,
+        distance: "30%",
+        origin: "right",
+        reset: true
+    })
+
+ScrollReveal()
+    .reveal('.home__img', {
+        duration: 1000,
+        delay: 400,
+        distance: "20%",
+        origin: "left",
+        reset: true
+    })
+ScrollReveal()
+    .reveal('.rates__center', {
+        duration: 1000,
+        delay: 400,
+        distance: "20%",
+        origin: "bottom",
+        reset: true
+    })
+ScrollReveal()
+    .reveal('.rates__right', {
+        duration: 1000,
+        delay: 400,
+        distance: "50%",
+        origin: "top",
+        reset: true
+    })
+ScrollReveal()
+    .reveal('.rates__left', {
+        duration: 1000,
+        delay: 400,
+        distance: "50%",
+        origin: "top",
+        reset: true
+    })
+
+    
 
